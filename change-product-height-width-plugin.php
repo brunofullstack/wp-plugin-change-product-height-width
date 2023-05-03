a460daefbaf1947cde05c0eea97a0cd477c6b904
Bruno Carvalho <bruno.carvalho@hotmail.de>
2023-05-03 15:58:43 -0300

    first

---
+++ change-product-height-width-plugin.php
@@ -0,0 +1,7 @@
+/**
+ * Plugin Name: Change product height width plugin
+ * Description: Cadastro global que se aplica a todos os produtos é possivel definir um limite máximo e mínimo da medida e também os valores de acordo com o tamanho.
+ * Version: 1.0.0
+ * Author: Bruno Carvalho
+ * Author URI: https://brunofullstack.github.io/
+ **/
