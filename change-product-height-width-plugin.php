c3e0c00b895d4b07d1ced5fb0bed6cb788221ea6
Bruno Carvalho <bruno.carvalho@hotmail.de>
2023-05-04 17:49:22 -0300

    feat: savinf values into db table

--- change-product-height-width-plugin.php
+++ change-product-height-width-plugin.php
@@ -8,12 +8,12 @@
  **/
 
 // Adiciona um novo campo no formulário de edição do produto
-add_action('woocommerce_product_options_general_product_data', 'meu_plugin_custom_field');
-function meu_plugin_custom_field()
+add_action('woocommerce_product_options_general_product_data', 'wp_g_h_w_plugin_custom_field');
+function wp_g_h_w_plugin_custom_field()
 {
     woocommerce_wp_text_input(
         array(
-            'id' => 'meu_plugin_custom_field',
+            'id' => 'wp_g_h_w_plugin_custom_field',
             'label' => __('Altura Inicial (cm)', 'meu-plugin'),
             'placeholder' => '',
             'desc_tip' => 'true',
@@ -22,7 +22,7 @@ function meu_plugin_custom_field()
     );
     woocommerce_wp_text_input(
         array(
-            'id' => 'meu_plugin_custom_field',
+            'id' => 'wp_g_h_w_plugin_custom_field',
             'label' => __('Largura Inicial (cm)', 'meu-plugin'),
             'placeholder' => '',
             'desc_tip' => 'true',
@@ -32,18 +32,18 @@ function meu_plugin_custom_field()
 }
 
 // Salva o valor do campo personalizado
-add_action('woocommerce_process_product_meta', 'meu_plugin_custom_field_save');
-function meu_plugin_custom_field_save($post_id)
+add_action('woocommerce_process_product_meta', 'wp_g_h_w_plugin_custom_field_save');
+function wp_g_h_w_plugin_custom_field_save($post_id)
 {
     $product = wc_get_product($post_id);
-    $custom_field = isset($_POST['meu_plugin_custom_field']) ? $_POST['meu_plugin_custom_field'] : '';
-    $product->update_meta_data('meu_plugin_custom_field', sanitize_text_field($custom_field));
+    $custom_field = isset($_POST['wp_g_h_w_plugin_custom_field']) ? $_POST['wp_g_h_w_plugin_custom_field'] : '';
+    $product->update_meta_data('wp_g_h_w_plugin_custom_field', sanitize_text_field($custom_field));
     $product->save();
 }
 
 // Adiciona uma página de configuração para o plugin
-add_action('admin_menu', 'meu_plugin_menu');
-function meu_plugin_menu()
+add_action('admin_menu', 'wp_g_h_w_plugin_menu');
+function wp_g_h_w_plugin_menu()
 {
     add_menu_page(
         'Configurações de preço/altura/largura',
@@ -54,30 +54,39 @@ function meu_plugin_menu()
         // permissão de acesso
         'meu-plugin-settings',
         // slug da página
-        'meu_plugin_settings_page' // função que renderiza a página
+        'wp_g_h_w_plugin_settings_page' // função que renderiza a página
     );
 }
 
 
 // Cria o conteúdo da página de configuração
-function meu_plugin_settings_page()
+function wp_g_h_w_plugin_settings_page()
 {
     if (!current_user_can('manage_options')) {
         return;
     }
 
-    $altura = get_option('meu_plugin_altura');
-    $largura = get_option('meu_plugin_largura');
-    $preco_base = get_option('meu_plugin_preco_base');
+    $altura = get_option('wp_g_h_w_plugin_altura');
+    $largura = get_option('wp_g_h_w_plugin_largura');
+    $preco_base_0_05 = get_option('wp_g_h_w_plugin_preco_base_0_05');
+    $preco_base_05_1 = get_option('wp_g_h_w_plugin_preco_base_05_1');
+    $preco_base_1_3 = get_option('wp_g_h_w_plugin_preco_base_1_3');
+    $preco_base_3_5 = get_option('wp_g_h_w_plugin_preco_base_3_5');
 
     if (isset($_POST['submit'])) {
-        $altura = $_POST['meu_plugin_altura'];
-        $largura = $_POST['meu_plugin_largura'];
-        $preco_base = $_POST['meu_plugin_preco_base'];
+        $altura = $_POST['wp_g_h_w_plugin_altura'];
+        $largura = $_POST['wp_g_h_w_plugin_largura'];
+        $preco_base_0_05 = $_POST['wp_g_h_w_plugin_preco_base_0_05'];
+        $preco_base_05_1 = $_POST['wp_g_h_w_plugin_preco_base_05_1'];
+        $preco_base_1_3 = $_POST['wp_g_h_w_plugin_preco_base_1_3'];
+        $preco_base_3_5 = $_POST['wp_g_h_w_plugin_preco_base_3_5'];
 
-        update_option('meu_plugin_altura', $altura);
-        update_option('meu_plugin_largura', $largura);
-        update_option('meu_plugin_preco_base', $preco_base);
+        update_option('wp_g_h_w_plugin_altura', $altura);
+        update_option('wp_g_h_w_plugin_largura', $largura);
+        update_option('wp_g_h_w_plugin_preco_base', $preco_base_0_05);
+        update_option('wp_g_h_w_plugin_preco_base', $preco_base_05_1);
+        update_option('wp_g_h_w_plugin_preco_base', $preco_base_1_3);
+        update_option('wp_g_h_w_plugin_preco_base', $preco_base_3_5);
     }
 
     ?>
@@ -86,22 +95,25 @@ function meu_plugin_settings_page()
             <?php echo esc_html(get_admin_page_title()); ?>
         </h1>
         <label for="ping_sites">
-            Cadastro global que se aplica a todos os produtos. Aqui é possivel definir um limite máximo e mínimo da medida e também os valores de acordo com o tamanho.
+            Cadastro global que se aplica a todos os produtos. Aqui é possivel definir um limite máximo e mínimo da medida e
+            também os valores de acordo com o tamanho.
         </label>
-        <form method="post">
+        <form method="post" action="salvar_informacoes">
+            <input type="hidden" name="action" value="salvar_informacoes">
+
             <h2>
                 Tamanho
             </h2>
             <table class="form-table">
                 <tbody>
                     <tr>
-                        <th scope="row"><label for="meu_plugin_altura">Altura máxima (cm):</label></th>
-                        <td><input name="meu_plugin_altura" type="number" step="0.01" id="meu_plugin_altura"
+                        <th scope="row"><label for="wp_g_h_w_plugin_altura">Altura máxima (cm):</label></th>
+                        <td><input name="wp_g_h_w_plugin_altura" type="number" step="0.01" id="wp_g_h_w_plugin_altura"
                                 value="<?php echo esc_attr($altura); ?>" class="regular-text"></td>
                     </tr>
                     <tr>
-                        <th scope="row"><label for="meu_plugin_largura">Largura máxima (cm):</label></th>
-                        <td><input name="meu_plugin_largura" type="number" step="0.01" id="meu_plugin_largura"
+                        <th scope="row"><label for="wp_g_h_w_plugin_largura">Largura máxima (cm):</label></th>
+                        <td><input name="wp_g_h_w_plugin_largura" type="number" step="0.01" id="wp_g_h_w_plugin_largura"
                                 value="<?php echo esc_attr($largura); ?>" class="regular-text"></td>
                     </tr>
                 </tbody>
@@ -112,24 +124,28 @@ function meu_plugin_settings_page()
             <table class="form-table">
                 <tbody>
                     <tr>
-                        <th scope="row"><label for="meu_plugin_preco_base">0 a 0,5M² em R$:</label></th>
-                        <td><input name="meu_plugin_preco_base" type="number" step="0.01" id="meu_plugin_preco_base"
-                                value="<?php echo esc_attr($preco_base); ?>" class="regular-text"></td>
+                        <th scope="row"><label for="wp_g_h_w_plugin_preco_base_0_05">0 a 0,5M² em R$:</label></th>
+                        <td><input name="wp_g_h_w_plugin_preco_base_0_05" type="number" step="0.01"
+                                id="wp_g_h_w_plugin_preco_base" value="<?php echo esc_attr($preco_base_0_05); ?>"
+                                class="regular-text"></td>
                     </tr>
                     <tr>
-                        <th scope="row"><label for="meu_plugin_preco_base">0,5 a 1M² em R$:</label></th>
-                        <td><input name="meu_plugin_preco_base" type="number" step="0.01" id="meu_plugin_preco_base"
-                                value="<?php echo esc_attr($preco_base); ?>" class="regular-text"></td>
+                        <th scope="row"><label for="wp_g_h_w_plugin_preco_base_05_1">0,5 a 1M² em R$:</label></th>
+                        <td><input name="wp_g_h_w_plugin_preco_base_05_1" type="number" step="0.01"
+                                id="wp_g_h_w_plugin_preco_base" value="<?php echo esc_attr($preco_base_05_1); ?>"
+                                class="regular-text"></td>
                     </tr>
                     <tr>
-                        <th scope="row"><label for="meu_plugin_preco_base">1 a 3M² em R$:</label></th>
-                        <td><input name="meu_plugin_preco_base" type="number" step="0.01" id="meu_plugin_preco_base"
-                                value="<?php echo esc_attr($preco_base); ?>" class="regular-text"></td>
+                        <th scope="row"><label for="wp_g_h_w_plugin_preco_base_1_3">1 a 3M² em R$:</label></th>
+                        <td><input name="wp_g_h_w_plugin_preco_base_1_3" type="number" step="0.01"
+                                id="wp_g_h_w_plugin_preco_base" value="<?php echo esc_attr($preco_base_1_3); ?>"
+                                class="regular-text"></td>
                     </tr>
                     <tr>
-                        <th scope="row"><label for="meu_plugin_preco_base">3 a 5M² em R$:</label></th>
-                        <td><input name="meu_plugin_preco_base" type="number" step="0.01" id="meu_plugin_preco_base"
-                                value="<?php echo esc_attr($preco_base); ?>" class="regular-text"></td>
+                        <th scope="row"><label for="wp_g_h_w_plugin_preco_base_3_5">3 a 5M² em R$:</label></th>
+                        <td><input name="wp_g_h_w_plugin_preco_base_3_5" type="number" step="0.01"
+                                id="wp_g_h_w_plugin_preco_base" value="<?php echo esc_attr($preco_base_3_5); ?>"
+                                class="regular-text"></td>
                     </tr>
                 </tbody>
             </table>
@@ -137,6 +153,81 @@ function meu_plugin_settings_page()
         </form>
     </div>
     <?php
+}
+function salvar_informacoes()
+{
+    global $wpdb;
+
+    // Apagar todas os registros anteriores para inseriro novo
+    // INSIRA O CÓDIGO AQUI
+    $sql = 'SELECT COUNT(*) FROM wp_global_height_width_plugin';
+    $counter = $wpdb->get_var($sql);
+    if ($counter > 0) {
+        $sql_limpar = 'TRUNCATE wp_global_height_width_plugin';
+        $wpdb->query($sql_limpar); // executa a consulta de limpeza
+    }
+
+    // Inserindo novo registro
+    $altura = $_POST['wp_g_h_w_plugin_altura'];
+    $largura = $_POST['wp_g_h_w_plugin_largura'];
+    $preco_base_0_05 = $_POST['wp_g_h_w_plugin_preco_base_0_05'];
+    $preco_base_05_1 = $_POST['wp_g_h_w_plugin_preco_base_05_1'];
+    $preco_base_1_3 = $_POST['wp_g_h_w_plugin_preco_base_1_3'];
+    $preco_base_3_5 = $_POST['wp_g_h_w_plugin_preco_base_3_5'];
+
+    try {
+        $wpdb->insert(
+            'wp_global_height_width_plugin',
+            array(
+                'id' => $altura,
+                'altura_maxima' => $altura,
+                'largura_maxima' => $largura,
+                'preco_0_05' => $preco_base_0_05,
+                'preco_05_1' => $preco_base_05_1,
+                'preco_1_3' => $preco_base_1_3,
+                'preco_3_5' => $preco_base_3_5,
+            ),
+            array('%s', '%s', '%s', '%s', '%s', '%s')
+        );
+
+        $wpdb->close();
+
+        echo "Obrigado por se cadastrar";
+        exit;
+    } catch (Exception $ex) {
+        // jump to this part
+        // if an exception occurred
+    }
+
+
+    // echo "Obrigado por se cadastrar";
+    exit;
+}
+
+add_action('admin_post_salvar_informacoes', 'salvar_informacoes');
+add_action('admin_post_nopriv_salvar_informacoes', 'salvar_informacoes');
+// END OF FORM
+
+
+// Para verificar se uma tabela existe no WordPress e criar uma nova tabela usando a função global $wpdb
+global $wpdb;
+
+$table_exists = $wpdb->query("SHOW TABLES LIKE 'wp_global_height_width_plugin'");
+if ($table_exists !== 1) {
+    $charset_collate = $wpdb->get_charset_collate();
+    $sql = "CREATE TABLE wp_global_height_width_plugin (
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        largura_maxima FLOAT NOT NULL,
+        altura_maxima FLOAT NOT NULL,
+        preco_0_05 DECIMAL(10, 2) NOT NULL,
+        preco_05_1 DECIMAL(10, 2) NOT NULL,
+        preco_1_3 DECIMAL(10, 2) NOT NULL,
+        preco_3_5 DECIMAL(10, 2) NOT NULL,
+        data_criacao DATETIME NOT NULL DEFAULT NOW(),
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
 }
 
 // add_action( 'template_redirect', 'my_callback' );
